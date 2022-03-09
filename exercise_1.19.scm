5215775c8e9f03f551f1d2fed4b853c8d9ecb59d
edvu <edvino.ur@gmail.com>
2022-03-09 23:17:14 +0200

    Start implement 1.19ex

---
+++ exercise_1.19.scm
@@ -0,0 +1,38 @@
+#lang sicp
+
+(define (even? n)
+  (= (remainder n 2) 0 ))
+
+(define (double x)
+  (+ x x))
+
+(define (halve x)
+  (/ x 2))
+
+;;;iterative solution with O(n) steps and O(1) space
+(define (fib n)
+  (fib-iter 1 0 n))
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   ; compute p ′
+                   ; compute q ′
+                   ⟨ ?? ⟩
+                   ⟨ ?? ⟩
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
